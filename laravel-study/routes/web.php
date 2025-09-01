fd199705703a3aa8da67aba6fb1bc737ae925ec5
satohirok <hiroki_g0421@yahoo.co.jp>
2025-09-02 07:10:03 +0900

    first commit

---
+++ laravel-study/routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UtilityController;
+
+Route::get('/hello-world', function () {
+    return view('hello_world', [
+        'name' => '佐藤',
+        'course' => 'Laravel Basics'
+    ]);
+});
+
+Route::get('/', function () {
+    return view('index', [
+        'name' => '佐藤',
+        'course' => 'Laravel Basics'
+    ]);
+});
+
+Route::get('/curriculum', function () {
+    return view('curriculum');
+});
+
+// 世界の時間
+Route::get('/world-time', [UtilityController::class, 'worldTime']);
+
+
+
+// おみくじ
+Route::get('/omikuji', function () {
+    $fortunes = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶'];
+    $resultIndex = array_rand($fortunes);
+    $result = $fortunes[$resultIndex];
+    return view('omikuji', ['result' => $result]);
+});
+
+// モンティ・ホール問題
+Route::get('/monty-hall', function () {
+    $results = [];
+    for ($i = 0; $i < 1000; $i++) {
+        $options = [true, false, false];
+        shuffle($options);
+
+        $selectedIndex = array_rand($options);
+        $notSelectedIndexes = array_filter($options, fn($index) => $index !== $selectedIndex, ARRAY_FILTER_USE_KEY);
+        $removeIndex = array_search(false, $notSelectedIndexes);
+        unset($notSelectedIndexes[$removeIndex]);
+
+        $changedIndex = key($notSelectedIndexes);
+        $results[] = $options[$changedIndex];
+    }
+    $wonCount = count(array_filter($results, fn($result) => $result));
+    return view('monty-hall', ['results' => $results, 'wonCount' => $wonCount]);
+});
